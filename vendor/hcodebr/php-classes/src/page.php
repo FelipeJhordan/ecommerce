abd551836693eab34de4d734d8c04de8e86a1993
Felipe Jhordan <felipejordan.alves@gmail.com>
2020-05-25 22:29:30 -0300

    ADMIN - Esqueceu a senha

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -8,12 +8,14 @@
 		private $tpl;
 		private $options = [];
 		private $defaults = [
+			"header"=>true,
+			"footer"=>true,
 			"data"=> []
 		];
-		public function __construct($opt = array()) {
+		public function __construct($opt = array(), $tpl_dir = "/views/") {
 			$this->options = array_merge($this->defaults, $opt);
 			$config = array(
-					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 					"debug"         => false // set to false to improve the speed
 				   );
@@ -21,7 +23,7 @@
 		Tpl::configure( $config );
 		$this->tpl = new Tpl();
 		$this->setData($this->options["data"]);
-		$this->tpl->draw("header");
+		if($this->options["header"])$this->tpl->draw("header");
 		}
 
 		public function setTpl($name,$data = array(), $returnHTML = false) { 
@@ -35,7 +37,7 @@
 			} 
 		}
 		public function __destruct() {
-			$this->tpl->draw("footer");
+			if($this->options["footer"])$this->tpl->draw("footer");
 		}
 	}
  ?>
