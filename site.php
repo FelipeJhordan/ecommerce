e978b4580e95db0bc1b59ba59fa7d40a14f48318
Felipe Jhordan <felipejordan.alves@gmail.com>
2020-06-10 00:38:19 -0300

    Adicionando 'minha conta' ao projeto

--- site.php
+++ site.php
@@ -253,4 +253,53 @@ $app->post("/forgot/reset",function() {
 	$page->setTpl("forgot-reset-success");
 
 });
+
+$app->get('/profile', function() {
+    User::verifyLogin(false);
+    $user = User::getFromSession();
+    $page = new Page();
+
+    $page->setTpl('profile', [
+        'user'=>$user->getValues(),
+        'profileMsg' => User::getSuccess(),
+        'profileError' => User::getError()
+    ]);
+});
+
+$app->post('/profile', function() {
+    User::verifyLogin(false);
+    $user = User::verifyLogin();
+
+    if(!isset($_POST['desperson']) || $_POST['desperson'] === '') {
+        User::setError("Preencha seu nome.");
+        header('Location: /profile');
+        exit;
+    }
+    if(!isset($_POST['desemail']) || $_POST['desemail'] === '') {
+        User::setError("Preencha seu email.");
+         header('Location: /profile');
+        exit;
+    }
+    $user = User::getFromSession();
+
+    if($_POST['desemail'] !== $user->getdesemail()){
+        if(User::checkLoginExist($_POST['desemail']) === true ) {
+            User::setError('Este endereço já está cadastrado');
+             header('Location: /profile');
+              exit;
+        }
+    }
+
+    $_POST['inadmin'] = $user->getinadmin();
+    $_POST['despassword'] = $user->getdespassword();
+    $_POST['deslogin'] = $_POST['desemail'];
+
+    $user->setData($_POST);
+
+    $user->update();
+    User::setSuccess("Usuário  alterado com sucesso!");
+    header('Location: /profile');
+    exit;
+})
+
 ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,7 @@
         const SECRET_IV = "HcodePhp7_Secret_IV";
         const ERROR = 'UserError';
         const ERROR_REGISTER = "UserErrorRegister";
+        const SUCCESS = 'UserSuccess';
         public static function login ( $login, $password ) { 
             $sql = new Sql();   
             $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
@@ -287,5 +288,21 @@
 
         return (count($results) > 0);
     }
+
+    public static function setSuccess($msg) {
+        $_SESSION[User::SUCCESS] = $msg;
+    }
+
+    public static function getSuccess(){
+        $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+        User::clearSuccess();
+
+        return $msg;
+    }
+
+    public static function clearSuccess(){
+        $_SESSION[User::SUCCESS] = NULL;
+    }
+
     }
 ?>
